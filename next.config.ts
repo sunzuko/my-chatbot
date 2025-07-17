86da6edba35fd590d27028d0e1f9b901319cec36
daiya <sunzuko@gmail.com>
2025-07-17 21:14:03 +0900

    fix eslint error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+[21:09:39.439] Running build in Washington, D.C., USA (East) – iad1
+[21:09:39.439] Build machine configuration: 2 cores, 8 GB
+[21:09:39.466] Cloning github.com/sunzuko/my-chatbot (Branch: main, Commit: fbc05a2)
+[21:09:39.589] Previous build caches not available
+[21:09:39.708] Cloning completed: 242.000ms
+[21:09:41.353] Running "vercel build"
+[21:09:41.802] Vercel CLI 44.4.3
+[21:09:42.098] Installing dependencies...
+[21:09:53.538] 
+[21:09:53.538] added 343 packages in 11s
+[21:09:53.539] 
+[21:09:53.539] 140 packages are looking for funding
+[21:09:53.539]   run `npm fund` for details
+[21:09:53.592] Detected Next.js version: 15.4.1
+[21:09:53.597] Running "npm run build"
+[21:09:53.706] 
+[21:09:53.706] > my-chatbot@0.1.0 build
+[21:09:53.706] > next build
+[21:09:53.706] 
+[21:09:54.481] Attention: Next.js now collects completely anonymous telemetry regarding usage.
+[21:09:54.482] This information is used to shape Next.js' roadmap and prioritize features.
+[21:09:54.482] You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
+[21:09:54.482] https://nextjs.org/telemetry
+[21:09:54.482] 
+[21:09:54.584]    ▲ Next.js 15.4.1
+[21:09:54.585] 
+[21:09:54.613]    Creating an optimized production build ...
+[21:10:06.501]  ✓ Compiled successfully in 8.0s
+[21:10:06.506]    Linting and checking validity of types ...
+[21:10:10.131] 
+[21:10:10.132] Failed to compile.
+[21:10:10.132] 
+[21:10:10.132] ./app/page.tsx
+[21:10:10.132] 7:44  Error: Unexpected any. Specify a different type.  @typescript-eslint/no-explicit-any
+[21:10:10.132] 
+[21:10:10.132] info  - Need to disable some ESLint rules? Learn more here: https://nextjs.org/docs/app/api-reference/config/eslint#disabling-rules
+[21:10:10.167] Error: Command "npm run build" exited with 1
+[21:10:10.364] 
+[21:10:13.176] Exiting build container
